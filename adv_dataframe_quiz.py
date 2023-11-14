bed56871065675f650b8ad881504f9f60f5923fb
casey <cpmoden@gmail.com>
2023-11-13 21:59:23 -0500

    Finish

--- adv_dataframe_quiz.py
+++ adv_dataframe_quiz.py
@@ -6,19 +6,18 @@ def remove_empty_col(df):
 def remove_any_missing_row(df):
     pass
 
-def change_data_type(df):
+def mean_score(df):
     pass
 
 def merge_data(left, right):
     pass
 
 def main():
-    test_data = pd.read_csv("student_data.csv")
-    print(test_data)
+    test_data = pd.read_csv("student_test_data.csv")
     test_data = remove_empty_col(test_data)
     test_data = remove_any_missing_row(test_data)
-    test_data = change_data_type(test_data)
-    
+    average = mean_score(test_data)
+
     demographic_data = pd.read_csv("student_demo_data.csv")
     merged_data = merge_data(test_data, demographic_data)
     
@@ -29,7 +28,7 @@ def main():
     
     
     
-    print(df)
+    
 
 if __name__ == "__main__":
     main()
